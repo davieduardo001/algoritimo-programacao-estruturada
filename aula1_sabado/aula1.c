c1c5e041bed5827aa274c9b6bf524fabc24e649c
DAVI EDUARDO COSTA MIRANDA <davi.emiranda@a.ucb.br>
2023-08-05 11:17:18 -0300

    ...

---
+++ aula1_sabado/aula1.c
@@ -0,0 +1,19 @@
+/*
+    Bibliotecas (funcoes pre prontas dentro da linguagem)
+    importacao de uma biblioteca
+*/
+
+//diretiva / importacao de lib
+#include<stdio.h>
+
+//Funcao main da linguagem C (funcao nativa)
+int main(){
+    //main block function
+
+    //imprimir alguma coisa na tela, funcao nativa da linguagem C
+    printf("bem vindos a diciplina\n"); //conteudo dentro do parenteses eh um parametro (eh um parametro de texto que vai passar para a funcao processar e fazer o output)
+    printf("bem vindos a diciplina\n");
+    printf("bem vindos a diciplina\n");
+
+    return 0;
+}
