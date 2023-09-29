3f1a82cfe340a781a0f100f8d32ee4b97f24d785
davieduardo001 <ed.miranda@tutanota.com>
2023-09-29 15:47:59 -0400

    ..

--- aula9_sabado/nha.c
+++ aula9_sabado/nha.c
@@ -1,17 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[ ]) {
+int main(int argc, char *argv[])
+{
 
     int i;
-    
+
     printf("\n\n    ( ");
     for (i = 0; i < argc; i++)
     {
-        if (i>0)
+        if (i > 0)
             printf("%s ", argv[i]);
     }
     printf(")\n");
-
 
     printf("        /\n       /\n");
     printf("┈┈╱╲┈┈┈╱╲┈┈╭━╮┈\n┈╱╱╲╲__╱╱╲╲┈╰╮┃┈\n┈▏┏┳╮┈╭┳┓▕┈┈┃┃┈\n┈▏╰┻┛▼┗┻╯▕┈┈┃┃┈\n┈╲┈┈╰┻╯┈┈╱▔▔┈┃┈\n┈┈╰━┳━━━╯┈┈┈┈┃┈\n┈┈┈┈┃┏┓┣━━┳┳┓┃┈\n┈┈┈┈┗┛┗┛┈┈┗┛┗┛┈\n");
