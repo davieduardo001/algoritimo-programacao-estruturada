e9f58f7e035ff78286886b9294f18040c6923eef
DAVI EDUARDO COSTA MIRANDA <davi.emiranda@a.ucb.br>
2023-11-01 11:19:44 -0300

    push

--- AVALIACAO2/ex1/main.c
+++ AVALIACAO2/ex1/main.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 #include "src.h"
 
 int main() {
+    struct funcionario funcionarios[10000];
+
+    receberQuantidadeVendidos(funcionarios);
+
+    printf("\nIMPRIMINDO FUNCIONARIOS: \n");
+    imprimindoFuncionario(funcionarios);
+
     return 0;
 }
--- AVALIACAO2/ex1/src.h
+++ AVALIACAO2/ex1/src.h
@@ -1,47 +1,81 @@
-int receberQuantidadeVendidos() {
-    int maisUmFuncionario = 1;
-    int *contribuicaoFuncionarios, *salarioFuncionarios;
+struct funcionario {
+    float salario;
+    int index;
+    int contribuicaoFuncionario;
+};
 
-    contribuicaoFuncionarios = malloc(maisUmFuncionario * sizeof(int));
+int count = 0;
+
+float calculaSalarioMaisDe80(int contribuicoes) {
+    float total = 600;
+    int c = contribuicoes;
+    c -= 80;
+
+    total += 0.75*c;
+    total += 0.50*30;
+
+    return total;
+}
+
+float calculaSalarioMaisDe50(int contribuicoes) {
+    float total = 600;
+    int c = contribuicoes;
+    c -= 50;
+
+    total += 0.50*c;
+
+    return total;
+}
+
+float validaQuantidade(float contribuicoes) {
+    float salario = 0;
+
+    if(contribuicoes >= 50 && contribuicoes <= 80) {
+        salario = calculaSalarioMaisDe50(contribuicoes);
+    } else if(contribuicoes > 80) {
+        salario = calculaSalarioMaisDe80(contribuicoes);
+    } else {
+        salario = 600;
+    }
+
+    return salario;
+}
+
+int receberQuantidadeVendidos(struct funcionario *f) {
+    int i = 0;
+    int count = 1;
 
     while(1) {
-        printf("escreva o numero de contribuicoes do funcionario %i: ", *contribuicaoFuncionarios);
-        scanf("%i", &contribuicaoFuncionarios[]);
+        int n = 0;
+        int confirmacao;
 
-        //AAAAAAAAAAAAAAAAAAA
+        f[i].index = i;
+
+        printf("Coloque o numero de contribuicoes do funcionario [%i]: ", f[i].index);
+        scanf("%i", &n);
+
+        f[i].contribuicaoFuncionario = n;
+
+        f[i].salario = validaQuantidade(f[i].contribuicaoFuncionario);
+        
+        printf("Deseja adicionar mais um usuario? [1 para adiconar / 0 para sair] ");
+        scanf("%i", &confirmacao);
+
+        if(confirmacao == 1) {
+            count++;
+        } else {
+            break;
+        }
+
     }
 
 
 }
 
-int validaQuantidade(float salario, int quantidaeDePecas) {
-    float salarioFinal = salario + verificarBonusMais80(quantidaeDePecas) + verificarBonusMais50(quantidaeDePecas);
-
-    return salarioFinal;
-}
-
-float verificarBonusMais80(int quantidaeDePecas) {
-    int i;
-    float total;
-
-    quantidaeDePecas -= 80;
-
-    if(quantidaeDePecas > 0)
-        for(int i = 0; i < quantidaeDePecas; i++);
-            total += 0.75;
-    
-    return float;
-}
-
-float verificarBonusMais50(int quantidaeDePecas) {
-    int i;
-    float total;
-
-    quantidaeDePecas -= 50;
-
-    if(quantidaeDePecas > 0)
-        for(int i = 0; i < quantidaeDePecas; i++);
-            total += 0.50;
-    
-    return total;
+void imprimindoFuncionario(struct funcionario *f) {
+    for(int i = 0; i < count; i++) {
+        printf("Numero de contribuicoes: %i\n", f[i].contribuicaoFuncionario);
+        printf("Salario: %.2f\n", f[i].salario);
+        printf("Index: %i\n", f[i].index);
+    }
 }
